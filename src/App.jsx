12e9c391617a71eabe604e146843bca85e7febb9
Anark <dreckmannmoises@gmail.com>
2024-02-21 13:55:51 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import Logar from './pages/Logar.jsx';
 import Cadastro from './pages/Cadastro.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import userContext from './contexts/userContext.js';
 
 function App() {
+  const [user, setUser] = useState('');
+
+  const updateUser = (userName) => {
+    setUser(userName);
+  };
+
   return (
-    <userContext.Provider value={{user: '123'}}>
+    <userContext.Provider value={{ user }}>
       <Router>
         <main>
           <Routes>
-              <Route path="/projetoIfsulTemperatura/" index element={<Logar />} />
-              <Route path="/projetoIfsulTemperatura/cadastrar" element={<Cadastro />} />
-              <Route path="/projetoIfsulTemperatura/dashboard" element={<Dashboard />} />
+            <Route path="/projetoIfsulTemperatura/" element={<Logar updateUser={updateUser} />} />
+            <Route path="/projetoIfsulTemperatura/cadastrar" element={<Cadastro updateUser={updateUser} />} />
+            <Route path="/projetoIfsulTemperatura/dashboard" element={<Dashboard />} />
           </Routes>
         </main>
       </Router>
--- src/pages/Logar.jsx
+++ src/pages/Logar.jsx
@@ -1,61 +1,60 @@
+import React from 'react';
 import Button from '../components/Button.jsx';
 import Input from '../components/Input.jsx';
 import Navbar from '../components/NavBar.jsx';
-import { useForm } from 'react-hook-form'
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as Yup from "yup"
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import { useNavigate } from 'react-router-dom';
 
+function Login({ updateUser }) {
+  const schema = Yup.object().shape({
+    email: Yup.string().required('O campo email é obrigatório'),
+    pass: Yup.string().required('O campo senha é obrigatório').min(8,'A senha precisa ter pelo menos 8 caracteres'),
+  });
 
-function Login() {
+  const { register, handleSubmit, formState, reset } = useForm({
+    mode: 'onSubmit', 
+    resolver: yupResolver(schema)
+  });
 
-    const schema = Yup.object().shape({
-      email: Yup.string().required('O campo email é obrigatório'),
-      pass: Yup.string().required('O campo senha é obrigatório').min(8,'A senha precisa ter pelo menos 8 caracteres'),
-    });
-  
+  const { errors } = formState;
+  const navigate = useNavigate();
 
-    const { register, handleSubmit, formState, reset } = useForm({
-      mode: 'onSubmit', 
-      resolver: yupResolver(schema)})
+  const handleSubmitData = (data) => {
+    const { email } = data;
+    const userName = email.split('@')[0];
+    updateUser(userName); // Atualiza o contexto do usuário com o nome digitado antes do arroba no email
+    reset();
+    navigate("/projetoIfsulTemperatura/dashboard");
+    return data;
+  }
 
-    const {errors} = formState
-    const navigate = useNavigate();
-    
+  return (
+    <form className='divLogin' onSubmit={handleSubmit(handleSubmitData)}>
+      <Input 
+        {...register('email', { pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/ })} 
+        type="email" 
+        placeholder='E-mail' 
+        maxLength={27}
+        name="email"
+      />
+      {errors.email && <span className='span1'>{errors.email.message}</span>}
 
-    const handleSubmitData = (data) => {
-      //console.log(data)
-      //dados para serem consumidos por api
-      reset()
-      navigate("/projetoIfsulTemperatura/dashboard");
-      return data
-    }
-
-    return(
-        <form className='divLogin' onSubmit={handleSubmit(handleSubmitData)}>
-          <Input 
-            {...register('email', { pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/ })} 
-            type="email" 
-            placeholder='E-mail' 
-            maxLength={27}
-            name="email"
-          />
-          {errors.email && <span className='span1'>{errors.email.message}</span>}
-
-          <Input 
-            {...register('pass', { pattern: /^.{8,}$/ })}
-            type="password" 
-            placeholder='Password' 
-            maxLength={10}
-            name="pass"
-          />
-          {errors.pass && <span className='span1'>{errors.pass.message}</span>}
-            
-          <Button type='submit' className="btn" nome="LOGAR"/>
-          <div className='divEspaco'></div>
-          <Navbar />
-      </form>
-    )
+      <Input 
+        {...register('pass', { pattern: /^.{8,}$/ })}
+        type="password" 
+        placeholder='Password' 
+        maxLength={10}
+        name="pass"
+      />
+      {errors.pass && <span className='span1'>{errors.pass.message}</span>}
+        
+      <Button type='submit' className="btn" nome="LOGAR"/>
+      <div className='divEspaco'></div>
+      <Navbar />
+    </form>
+  )
 }
 
-export default Login
+export default Login;
